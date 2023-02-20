c19b33e1b16b520ed4e42543b6b544835626cb11
Zulfi Rizkiawan <68777448+zulfirizkiawan@users.noreply.github.com>
2023-02-20 18:38:51 +0700

    landingPage Done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import "../styles/bootstrap.min.css";
+import "../styles/custom.css";
+
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+        />
+
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+          crossorigin="anonymous"
+          async
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
